358d22527508d37c9852e852bdb461afbb032da1
Serik1205 <barba_sl@i.ua>
2023-06-21 22:28:16 +0300

    upd tamplate-5

--- src/route/index.js
+++ src/route/index.js
@@ -3975,20 +3975,6 @@ router.get('/template-3', function (req, res) {
 })
 
 // ================================================================
-<<<<<<< HEAD
-// ================================================================
-
-// router.get Створює нам один ентпоїнт
-
-//           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/template-5', function (req, res) {
-	// res.render генерує нам HTML сторінку
-
-	//            ↙ cюди вводимо назву файлу з сontainer
-	res.render('template-5', {
-		layout: "template-5",
-=======
-
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/template-4', function (req, res) {
 	// res.render генерує нам HTML сторінку
@@ -3996,7 +3982,6 @@ router.get('/template-4', function (req, res) {
 	//            ↙ cюди вводимо назву файлу з сontainer
 	res.render('template-4', {
 		layout: "template-4",
->>>>>>> 8f8ca350d55a1631af3bf308de66636ab622eddd
 		page: {
 			title: "Example Page"
 		},
@@ -4016,7 +4001,6 @@ router.get('/template-4', function (req, res) {
 			}
 		],
 
-<<<<<<< HEAD
 		form: {
 			title: "Please sign in",
 
@@ -4075,8 +4059,6 @@ router.get('/template-4', function (req, res) {
 				text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
 			}
 		],
-
-=======
 		main: {
 			heading: "Oh yeah, it’s that good. See for yourself.",
 			descriotion: "Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.",
@@ -4126,8 +4108,6 @@ router.get('/template-4', function (req, res) {
 				url: "https://example.com/"
 			},
 		],
-
->>>>>>> 8f8ca350d55a1631af3bf308de66636ab622eddd
 		footer: [
 			{
 				title: "Features",
@@ -4193,9 +4173,161 @@ router.get('/template-4', function (req, res) {
 	//                  ↑↑ сюди вводимо JSON дані
 })
 
-<<<<<<< HEAD
 // ================================================================
-=======
->>>>>>> 8f8ca350d55a1631af3bf308de66636ab622eddd
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/template-5', function (req, res) {
+	// res.render генерує нам HTML сторінку
+
+	//            ↙ cюди вводимо назву файлу з сontainer
+	res.render('template-5', {
+		layout: "template-5",
+		page: {
+			title: "Example Page"
+		},
+
+		header: [
+			{
+				text: "Features",
+				url: "https://example.com/home"
+			},
+			{
+				text: "Enterprise",
+				url: "https://example.com/about"
+			},
+			{
+				text: "Support",
+				url: "https://example.com/services"
+			}
+		],
+
+		form: {
+			title: "Please sign in",
+
+			inputs: [
+				{
+					id: 1,
+					name: "emailAddress",
+					placeholder: "Email address",
+					type: "email"
+				},
+				{
+					id: 2,
+					name: "userPassword",
+					placeholder: "Password",
+					type: "password"
+				}
+			],
+
+			checkbox: {
+				label: "Remember me",
+				name: "remember"
+			},
+
+			button: "Sign in",
+
+			info: "Stay updated on new releases and features, guides, and case studies."
+		},
+
+		messages: {
+			title: "List groups",
+			list: [
+				{
+					name: "Tom",
+					message: "I just got back from a trip to Europe. It was amazing!...",
+					day: "Tue"
+				},
+				{
+					name: "Emily",
+					message: "I can't wait for the weekend. I'm planning to go hiking with some friends...",
+					day: "Wed"
+				}
+			]
+		},
+
+		advantages: [
+			{
+				title: "24/7 Availability",
+				text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the."
+			},
+			{
+				title: "10 Years of Expertise",
+				text: "Fill out all required fields using real data on the checkout page."
+			},
+			{
+				title: "Loyalty Program",
+				text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
+			}
+		],
+
+		footer: [
+			{
+				title: "Features",
+				links: [
+					{
+						text: "Cool stuff",
+						url: "https://example.com/cool-stuff"
+					},
+					{
+						text: "Random feature",
+						url: "https://example.com/random-feature"
+					},
+					{
+						text: "Team feature",
+						url: "https://example.com/team-feature"
+					},
+					{
+						text: "Stuff for developers",
+						url: "https://example.com/stuff-for-developers"
+					}
+				]
+			},
+			{
+				title: "Resources",
+				links: [
+					{
+						text: "Resource name",
+						url: "https://example.com/resource-name"
+					},
+					{
+						text: "Another resource",
+						url: "https://example.com/another-resource"
+					},
+					{
+						text: "Final resource",
+						url: "https://example.com/final-resource"
+					}
+				]
+			},
+			{
+				title: "About",
+				links: [
+					{
+						text: "Team",
+						url: "https://example.com/team"
+					},
+					{
+						text: "Locations",
+						url: "https://example.com/locations"
+					},
+					{
+						text: "Privacy",
+						url: "https://example.com/privacy"
+					},
+					{
+						text: "Terms",
+						url: "https://example.com/terms"
+					}
+				]
+			}
+		]
+	})
+	//                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
